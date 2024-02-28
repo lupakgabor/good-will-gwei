07a81e5a1e7d65f6d15c50db1a5240557821df3d
Gabor Lupak <lupakgabor@gmail.com>
2024-02-28 21:16:55 +0100

    Create backend auto deploy github action

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,16 +1,19 @@
 import 'dotenv/config'
-import { HardhatUserConfig } from "hardhat/config";
+import {HardhatUserConfig} from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
-  networks: {
-    sepolia: {
-      url: process.env.SEPOLIA_RPC_URL,
-      accounts: [process.env.PRIVATE_KEY!],
+    solidity: "0.8.19",
+    networks: {
+        sepolia: {
+            url: process.env.SEPOLIA_RPC_URL,
+            accounts: [process.env.PRIVATE_KEY!],
+        },
+        etherscan: {
+            apiKey: process.env.ETHERSCAN_API_KEY,
+        },
     }
-  }
 };
 
 
