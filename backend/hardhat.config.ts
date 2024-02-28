ea703de6f7f6677916e8d9a68bb7168e9ef6847f
Gabor Lupak <lupakgabor@gmail.com>
2024-02-28 21:18:31 +0100

    Fix hardhat etherscan api key settings

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -10,10 +10,10 @@ const config: HardhatUserConfig = {
             url: process.env.SEPOLIA_RPC_URL,
             accounts: [process.env.PRIVATE_KEY!],
         },
-        etherscan: {
-            apiKey: process.env.ETHERSCAN_API_KEY,
-        },
-    }
+    },
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_API_KEY,
+    },
 };
 
 
